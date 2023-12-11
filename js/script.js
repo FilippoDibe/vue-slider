9f313ef00c711635d02f40bfbf4f798d870cee26
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-12-11 16:56:50 +0100

    add v-for in item class

--- js/script.js
+++ js/script.js
@@ -32,12 +32,8 @@ createApp({
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
             ],
-            currentI:0,
+            
         }
     },
-    // methods: {
-    //     changeSlide(i) {
-    //         this.currentI = i;
-    //     },
-    // },
+   
 }).mount('#app')
